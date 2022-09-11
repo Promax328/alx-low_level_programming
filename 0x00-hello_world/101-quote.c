fa67c64f525f3b8ac520bdb2f654f6983ef600bb
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-09-11 04:44:58 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Displys a string to standard error
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
