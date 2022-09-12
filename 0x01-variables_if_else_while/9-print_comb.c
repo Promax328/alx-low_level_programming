a67ce5ec19371f35131e4d5fb5d44b0536f83219
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-09-12 16:13:37 -0700

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - displays numbers 0 to 9 with commas in between
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int o;
+
+	for (o = '0'; o <= '9'; o++)
+	{
+		putchar(o);
+
+		if (!(o == '9'))
+		{
+			putchar(',');
+		}
+
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
