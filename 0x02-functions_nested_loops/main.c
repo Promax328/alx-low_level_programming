cc6b1d7a5a9e7d0a98a8a6fc25fb35ae013d07ed
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-09-14 14:04:13 -0700

    Create 3-islower.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -2,8 +2,16 @@
 
    int main(void)
 {
+	 int r;
 
-	 print_alphabet_x10();
+	 r = _islower('H');
+	 _putchar(r + '0');
+	 r = _islower('o');
+	 _putchar(r + '0');
+	 r = _islower(108);
+	 _putchar(r + '0');
+	 _putchar('\n');
+
          return (0);
 
 }
