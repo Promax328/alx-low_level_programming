4f5e4986dd15003b027826c7b4edc1a2ae31597c
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-09-14 16:51:21 -0700

    Create 4-isalpha.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -4,11 +4,11 @@
 {
 	 int r;
 
-	 r = _islower('H');
+	 r = _isalpha('H');
 	 _putchar(r + '0');
-	 r = _islower('o');
+	 r = _isalpha('o');
 	 _putchar(r + '0');
-	 r = _islower(108);
+	 r = _isalpha(108);
 	 _putchar(r + '0');
 	 _putchar('\n');
 
