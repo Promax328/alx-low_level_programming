0a6196d8e324be8e97f97117f0927821eabc9f0c
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-09-19 04:34:08 -0700

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line on the termainal
+ * @n: number of times to print the line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
+	}
+}
