df7ef2d844a8d2b306c8ce8bd3997c57a6de112c
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-09-24 06:35:42 -0700

    Create 5-rev_string.c

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,18 +1,33 @@
 #include "main.h"
+
+#include <stdio.h>
+
+
+
 /**
+
    * main - check the code
+
     *
+
      * Return: Always 0.
+
       */
 
 int main(void)
+
 {
-	    char *str;
 
-	        str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	    char s[10] = "My School";
 
-		    print_rev(str);
 
-		        return (0);
+
+	        printf("%s\n", s);
+
+		    rev_string(s);
+
+		        printf("%s\n", s);
+
+			    return (0);
 
 }
