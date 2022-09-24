6882b9c4ae35b2fb3b9fcd7a2e6dae4e191e7e67
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-09-24 07:09:12 -0700

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a 
+ * puts2 - prints every other character of a
  * string starting with the first character
  * @str: pointer to the str
  *
