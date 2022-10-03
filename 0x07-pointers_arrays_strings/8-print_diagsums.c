7284f9f534577a3e15cc5a5206feadf8b6999211
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-03 06:30:52 -0700

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+ * @a: pointrer to int array
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, priSum = 0, secSum = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		/*accesses the elements in the first 1D of the 2D array*/
+		/* i reps the column for each for each row*/
+		priSum += a[i];
+
+		secSum += a[size - 1 - i];
+		/*changes from one row(1D of the 2D) to another*/
+		a += size;
+	}
+
+	printf("%d, %d\n", priSum, secSum);
+}
+
