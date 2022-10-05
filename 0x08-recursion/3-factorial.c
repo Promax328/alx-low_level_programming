e0842e7a05a61b9aac81d7acd0952ac3c7544343
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-05 09:06:09 -0700

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,7 +4,7 @@
  * factorial - returns the factorial of a given number
  * @n: number to get factorial for
  *
- * Return: -1 is n is less than 0 or 1 if n is equal to 0 or 
+ * Return: -1 is n is less than 0 or 1 if n is equal to 0 or
  * the factorial of the n if greater than 0
  */
 
