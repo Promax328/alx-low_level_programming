46e032cf23b2b96a9f5093640c434d8f09a1aeeb
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-06 14:37:43 -0700

    fix betty error

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,6 +9,8 @@
 
 int strlen_recursion(char *a)
 {
+	/*gets the length of the string which will server*/
+	/*as end of the string*/
 	if (*a == '\0')
 		return (0);
 	else
@@ -19,24 +21,33 @@ int strlen_recursion(char *a)
 /**
  * checkPalindrome - checks if a string is the same if reversed
  * @c: pointer to string
+ * @end: integer of length of the string
+ * @start: integer number works together with end to
+ * check if strings are equal
  *
  * Return: 1 if true otherwise 0
  */
 
 int checkPalindrome(char *c, int end, int start)
 {
+	/*for start to be = end with this condition*/
+	/*it implies all elements of the string are equal*/
 	if (start >= end && c[start] == c[end])
 		return (1);
 
 	else if (c[start] != c[end])
 		return (0);
 
+	/*when both conditions above non is meet the func*/
+	/*recalls itself with the next elements of end*/
+	/*and start*/
 	else
 		return (checkPalindrome(c, end - 1, start + 1));
 }
 
 /**
- * is_palindrome - function that returns 1 if a string is a palindrome and 0 if not
+ * is_palindrome - function that returns 1 if a
+ * string is a palindrome and 0 if not
  * @s: pointer to string
  *
  * Return: 1 is true otherwise 0
