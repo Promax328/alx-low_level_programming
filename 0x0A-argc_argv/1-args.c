46b522955745e4f69bcdd8ce1c5354f74960b359
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-09 16:39:45 -0700

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguements  passed to it
+ * @argc: count of passed arguements
+ * @argv: pointer to string of passed arguements
+ *
+ * Return: always success (EXIT_SUCCESS)
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	exit(EXIT_SUCCESS);
+}
