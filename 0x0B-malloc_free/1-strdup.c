e9583293ee75102dc6ec743bf8fdadc29cc5e5eb
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-12 06:35:05 -0700

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,19 @@ char *_strdup(char *str)
 	int len, k;
 	char *s;
 
+	/*check if the str is NULL*/
 	if (str == NULL)
 		return (NULL);
 
+	/*gets the length of the given string*/
 	for (len = 0; str[len] != '\0'; len++)
 		;
 
+	/*allocates a space for the duplicate*/
+	/*string base on the length of str*/
 	s = malloc(sizeof(char) * len + 1);
+
+	/*checks if malloc is NULL*/
 	if (s == NULL)
 		return (NULL);
 
