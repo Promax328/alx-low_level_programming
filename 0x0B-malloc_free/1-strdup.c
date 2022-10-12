0f3b4596c76ff658cedb28c52624b38f09fa2317
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-12 06:28:21 -0700

    Create 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,7 +21,7 @@ char *_strdup(char *str)
 	for (len = 0; str[len] != '\0'; len++)
 		;
 
-	s = malloc(sizeof(char) * len);
+	s = malloc(sizeof(char) * len + 1);
 	if (s == NULL)
 		return (NULL);
 
