d60b4f6c02f2cf0546c06d8333589d180cf78bba
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-12 19:39:10 -0700

    Update  1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,24 +20,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	/*finds length for s1*/
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
-
+	/*finds length for s2*/
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 	if (n >= len2)
 		n = len2;
-
+	/*allocate memory based on len1, n and any extra*/
+	/* 1 for '\0' */
 	ptr = malloc(sizeof(char) * (len1 + n + 1));
+	/* test if malloc is NUUL*/
 	if (ptr == NULL)
 		return (NULL);
-
+	/*assign s1 to ptr excluding '\0' */
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-
+	/* assign s2 to ptr based on n */
+	/* and without '\0' */
 	for (k = 0; k < n; k++)
 		ptr[i + k] = s2[k];
-	
+	/*assign '\0' to ptr at the end of the memory*/
 	ptr[i + k] = '\0';
 	return (ptr);
 }
