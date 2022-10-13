457e9b984419c8fe6c6646123596e54d81424c36
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-13 13:49:23 -0700

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size > old_size)
+	if (new_size > old_size && ptr != NULL)
 	{
 		ptr2 = malloc(new_size);
 		if (ptr2 == NULL)
