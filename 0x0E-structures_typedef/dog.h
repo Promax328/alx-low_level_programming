7eebdb2be73dbea1cd1c3da28633ef33b4810bca
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-15 16:13:53 -0700

    Add structure documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - holds information for dogs and it owners
+ * @name: string for name of dog
+ * @age: float for age of the dog
+ * @owner: string for name of owwner
+ *
+ */
 struct dog
 {
 	char *name;
