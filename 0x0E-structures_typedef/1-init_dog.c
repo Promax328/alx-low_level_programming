bd32099e420c71489a3db2f80dcc4a808b8e9db5
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-15 16:52:03 -0700

    Update betty style for 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -11,6 +12,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
