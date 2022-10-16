7068d993cf9557d94c253dc4254e41701b88fb4e
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-15 19:19:53 -0700

    Create 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/*if it falls*/
 	new = malloc(sizeof(dog_t));
 	if (new == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 	new->name = name;
 	new->age = age;
 	new->owner = owner;
