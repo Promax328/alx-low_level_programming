cf34d69e4c5e54db34f912b0f324ad3c3155e7d7
promise_Ivienagbor <111024397+Promax328@users.noreply.github.com>
2022-10-15 17:16:40 -0700

    Update betty style for 1-init_dog.ci

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,9 +12,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
+	/*check if it is not yet allocated memory*/
 	if (d == NULL)
-		return (NULL);
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
